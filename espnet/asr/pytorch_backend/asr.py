f262fcd7148e530156febad0fe2cc81092da70b7
dali-dl <dali.darrenli@gmail.com>
2020-05-26 11:26:18 +0100

    fix the lipschiz constraint bug

--- espnet/asr/pytorch_backend/asr.py
+++ espnet/asr/pytorch_backend/asr.py
@@ -283,7 +283,7 @@ class CustomPGMUpdater(CustomUpdater):
 
         t = w - w_pretrained.cuda()
         norms = torch.sum(torch.abs(t), dim=axes, keepdim=True)
-        t = t * (1.0 / torch.max(1.0, norms / max_k))
+        t = t * (1.0 / torch.max(torch.ones_like(norms), norms / max_k))
         w = t + w_pretrained
         return w
 
